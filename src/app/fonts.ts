4fc67520f3f745f2427f8b025f768fc7ed3bdc9d
Joshua Barber <josh+github@jwjb.dev>
2024-07-13 15:55:16 +0100

    INIT

---
+++ src/app/fonts.ts
@@ -0,0 +1,5 @@
+import {Crimson_Text, Parisienne} from "next/font/google";
+
+export const crimsonText = Crimson_Text({subsets: ["latin"], weight: ["400", "600"]});
+
+export const parisienne = Parisienne({subsets: ["latin"], weight: ["400"]});
